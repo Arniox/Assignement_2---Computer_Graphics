32a31bf6412d483e2fc3c7daee9f6ea383a7a31b
Arniox <nikkdiehl@gmail.com>
2019-04-16 18:37:18 +1200

    Cleared up and segregated the drawing code
     - Each main part is in it's own function.
     - Functions are private
     - Fins are split into two functions (left and right). This allows for custom rotation control over each one as an entire object
     - Easier control over every part of the whale

--- Assignment 2/src/itemCreation/Fish.java
+++ Assignment 2/src/itemCreation/Fish.java
@@ -84,6 +84,38 @@ public class Fish {
 		this.fishPosition = globalPos;
 		this.fishRotation = globalRotation;
 		
+		//Push
+		gl.glPushMatrix();
+		//GLOBAL TRANSLATION AND ROTATIONS
+		gl.glTranslated(fishPosition[0], fishPosition[1], fishPosition[2]);
+		gl.glRotated(fishRotation[0], fishRotation[1], fishRotation[2], fishRotation[3]);
+		
+			//Set up main parts
+			this.setMainBodyParts();
+			this.setEyeParts();
+			
+			//Rotations for right fin animations
+			gl.glPushMatrix();
+			gl.glRotated(-0, 0, 0, 1);
+				this.setRightFinParts();
+			gl.glPopMatrix();
+			
+			//Rotations for left fin animations
+			gl.glPushMatrix();
+			gl.glRotated(0, 0, 0, 1);
+				this.setLeftFinParts();		
+			gl.glPopMatrix();
+
+		//Pop
+		gl.glPopMatrix();
+			
+	}
+	
+	/**
+	 * Sets up the main body parts. This includes body, teeth and mouth parts. Splitting up the drawing of the fish parts allows for easier control
+	 * @author Nikkolas Diehl
+	 */
+	private void setMainBodyParts() {
 		//Extra rotations and translation clipping for Body, mouth and teeth
 		//Teeth
 		ArrayList<float[]> extraTopTeethRotations = new ArrayList<float[]>();
@@ -98,13 +130,6 @@ public class Fish {
 		extraTopMouthClipping.add(new float[]{0,-0.03f,0});
 		ArrayList<float[]> extraTopMouthRotation = new ArrayList<float[]>();
 		extraTopMouthRotation.add(new float[]{1.5f,1,0,0});
-		
-		//Push
-		gl.glPushMatrix();
-		
-		//GLOBAL TRANSLATION AND ROTATIONS
-		gl.glTranslated(fishPosition[0], fishPosition[1], fishPosition[2]);
-		gl.glRotated(fishRotation[0], fishRotation[1], fishRotation[2], fishRotation[3]);
 		
 		//Set parts - Body, mouth, teeth
 		//Cylinder = Scale, Clipping options, Base radius, Top radius, Height of cylinder, Size scale, Translation, Color4d, Extra rotations, Extra translations
@@ -127,7 +152,13 @@ public class Fish {
 		fishTopBodyMouth.drawSphere(SCALE, new boolean[]{false, true, true, false}, 0.0584f, new double[]{1,1,3.25},
 										 new double[]{0,0,0},
 										 FISH_TOP_BODY_COLOUR, extraTopMouthRotation, extraTopMouthClipping);
-		
+	}
+	
+	/**
+	 * Sets up the eye parts. This includes the eye balls and eye lids. Splitting up the drawing of the fish parts allows for easier control
+	 * @author Nikkolas Diehl
+	 */
+	private void setEyeParts() {
 		//Extra rotations and translation clipping for Eyes
 		//EyeLidRight
 		ArrayList<float[]> extraEyeRightLidRotations = new ArrayList<float[]>();
@@ -154,51 +185,71 @@ public class Fish {
 		eyeBallLeft.drawSphere(SCALE, new boolean[]{false, false, false, false}, 0.004f, new double[]{0.8,0.8,0.8},
 										 new double[]{-0.047,0.0027,0.01},
 										 FISH_EYE_BALL_COLOUR, null, null);
-		
+	}
+	
+	/**
+	 * Sets up the right front fin parts. This includes the right and left inner fins, right and left middle fin parts and the right and left outer fin parts.<br>Splitting up the drawing of the fish parts allows for easier control
+	 * @author Nikkolas Diehl
+	 */
+	private void setRightFinParts() {
 		//Extra rotations and translation clipping for front fins
 		ArrayList<float[]> extraRightFinRotations = new ArrayList<float[]>();
 		extraRightFinRotations.add(new float[]{90,0,1,0});
 		extraRightFinRotations.add(new float[]{10,0,0,1});
-		ArrayList<float[]> extraLeftFinRotations = new ArrayList<float[]>();
-		extraLeftFinRotations.add(new float[]{-90,0,1,0});
-		extraLeftFinRotations.add(new float[]{-10,0,0,1});
 		
 		//Animation referencing
 		double[] finRightInnerTranslations = {0.045,0,0.06};
-		double[] finLeftInnerTranslations = {-0.045,0,0.06};
 		//Middle fin is referenced off of inner fin
 		double[] finRightMiddleTranslations = {finRightInnerTranslations[0]+0.0085,finRightInnerTranslations[1],finRightInnerTranslations[2]-0.0005};
-		double[] finLeftMiddleTranslations = {finLeftInnerTranslations[0]-0.0085,finLeftInnerTranslations[1],finLeftInnerTranslations[2]-0.0005};
-		//End fin is referenced off of middle fin
 		double[] finRightEndTranslations = {finRightMiddleTranslations[0]+0.0478,finRightMiddleTranslations[1]-0.0018,finRightMiddleTranslations[2]+0.0081};
-		double[] finLeftEndTranslations = {finLeftMiddleTranslations[0]-0.0478,finLeftMiddleTranslations[1]-0.0018,finLeftMiddleTranslations[2]+0.0081};
 		
 		//Set parts - Front fins
 		//Cylinder = Scale, Clipping options, Base radius, Top radius, Height of cylinder, Size scale, Translation, Color4d, Extra rotations, Extra translations
 		//Cone = Scale, Clipping options, Base radius, Height, Size scale, Translation, Colour4d, Extra rotations, Extra clipping
 		finRightInner.drawCylinder(SCALE, new boolean[]{false, false}, 0.001f, 0.008f, 0.01f, new double[]{1,0.2,1},
 										 finRightInnerTranslations, FISH_TOP_BODY_COLOUR, extraRightFinRotations, null);
-		finLeftInner.drawCylinder(SCALE, new boolean[]{false, false}, 0.001f, 0.008f, 0.01f, new double[]{1,0.2,1},
-										 finLeftInnerTranslations, FISH_TOP_BODY_COLOUR, extraLeftFinRotations, null);
 		//Rotations and translations of middle fin is referenced off of the inner fin
 		extraRightFinRotations.add(new float[]{-10,0,1,0});
 		finRightMiddle.drawCylinder(SCALE, new boolean[]{false, false}, 0.0076f, 0.007f, 0.05f, new double[]{1,0.2,1},
 										 finRightMiddleTranslations, FISH_TOP_BODY_COLOUR, extraRightFinRotations, null);
-		extraLeftFinRotations.add(new float[]{10,0,1,0});
-		finLeftMiddle.drawCylinder(SCALE, new boolean[]{false, false}, 0.0076f, 0.007f, 0.05f, new double[]{1,0.2,1},
-										 finLeftMiddleTranslations, FISH_TOP_BODY_COLOUR, extraLeftFinRotations, null);
 		//Rotations and translations of the end fin is referenced off of the middle fin
 		extraRightFinRotations.add(new float[]{-12,0,1,0});
 		finRightEnd.drawCone(SCALE, new boolean[]{false, false, false, false}, 0.0073f, 0.025f, new double[]{1,0.2,1},
 										 finRightEndTranslations, FISH_TOP_BODY_COLOUR, extraRightFinRotations, null);
+		
+	}
+	
+	/**
+	 * Sets up the right front fin parts. This includes the right and left inner fins, right and left middle fin parts and the right and left outer fin parts.<br>Splitting up the drawing of the fish parts allows for easier control
+	 * @author Nikkolas Diehl
+	 */
+	private void setLeftFinParts() {
+		//Extra rotations and translation clipping for front fins
+		ArrayList<float[]> extraLeftFinRotations = new ArrayList<float[]>();
+		extraLeftFinRotations.add(new float[]{-90,0,1,0});
+		extraLeftFinRotations.add(new float[]{-10,0,0,1});
+
+		//Animation referencing
+		double[] finLeftInnerTranslations = {-0.045,0,0.06};
+		//Middle fin is referenced off of inner fin
+		double[] finLeftMiddleTranslations = {finLeftInnerTranslations[0]-0.0085,finLeftInnerTranslations[1],finLeftInnerTranslations[2]-0.0005};
+		//End fin is referenced off of middle fin
+		double[] finLeftEndTranslations = {finLeftMiddleTranslations[0]-0.0478,finLeftMiddleTranslations[1]-0.0018,finLeftMiddleTranslations[2]+0.0081};
+		
+		//Set parts - Front fins
+		//Cylinder = Scale, Clipping options, Base radius, Top radius, Height of cylinder, Size scale, Translation, Color4d, Extra rotations, Extra translations
+		//Cone = Scale, Clipping options, Base radius, Height, Size scale, Translation, Colour4d, Extra rotations, Extra clipping
+		finLeftInner.drawCylinder(SCALE, new boolean[]{false, false}, 0.001f, 0.008f, 0.01f, new double[]{1,0.2,1},
+				 						 finLeftInnerTranslations, FISH_TOP_BODY_COLOUR, extraLeftFinRotations, null);
+		//Rotations and translations of middle fin is referenced off of the inner fin
+		extraLeftFinRotations.add(new float[]{10,0,1,0});
+		finLeftMiddle.drawCylinder(SCALE, new boolean[]{false, false}, 0.0076f, 0.007f, 0.05f, new double[]{1,0.2,1},
+										 finLeftMiddleTranslations, FISH_TOP_BODY_COLOUR, extraLeftFinRotations, null);
+		//Rotations and translations of the end fin is referenced off of the middle fin
 		extraLeftFinRotations.add(new float[]{12,0,1,0});
 		finLeftEnd.drawCone(SCALE, new boolean[]{false, false, false, false}, 0.0073f, 0.025f, new double[]{1,0.2,1},
-										 finLeftEndTranslations, FISH_TOP_BODY_COLOUR, extraLeftFinRotations, null);
+										 finLeftEndTranslations, FISH_TOP_BODY_COLOUR, extraLeftFinRotations, null);	
 		
-
-		//Pop
-		gl.glPopMatrix();
-			
 	}
 	
 }
